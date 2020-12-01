6a96aefb0cfe5d655941d2777ec016ad9e91fbf0
Richard Ojo <richardojo04@gmail.com>
2020-12-01 20:54:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,27 +1,38 @@
 def roll_call_dwarves(array)
-  roll_call_dwarves = []
-  array.each_with_index do |name, index| roll_call_dwarves.push("#{index + 1}. #{name}")
+   roll_call_dwarves = []
+   half = array.length / 2 
+  array[0...half].each_with_index do |name, index| roll_call_dwarves.push("#{index + 1}. #{name}")
   
 end
 puts " #{roll_call_dwarves.join(" ")} "
 end
 
 def summon_captain_planet(array)
- new_calls = array.collect do |call| 
-    "#{call.capitalize}!"
+ new_calls = array.collect do |element| 
+    "#{element.capitalize}!"
   end
   new_calls
 end
 
 def long_planeteer_calls(array)
-  array.any? do |word| 
-    word.length > 4 
+  array.any? do |element| 
+    element.length > 4 
   end 
 end 
 
 def find_the_cheese(array)
-  array.find do |ingredient|
+  array.find do |element|
     cheese_types = ["cheddar", "gouda", "camembert"]
-    cheese_types.include?(ingredient)
+    cheese_types.include?(element)
   end
 end
+
+
+def words_with_b(array)
+  new_words = []
+  array.each{|word|
+  if word[0].include?("b") 
+    new_words << word 
+  end 
+  new_word 
+end 
